7fae39611dbd07771b2afdb269c56e5051d722c6
kcrong <kcrong@kim82536.pe.kr>
2016-08-03 17:26:11 +0900

    Add calc time dec

--- main.py
+++ main.py
@@ -6,7 +6,18 @@ with open('nums.txt', 'r') as f:
 nums = [int(i.split('\n')[0]) for i in raw_nums]
 
 
+class CalcTime:
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, *args, **kwargs):
+        before = time()
+        self.func(*args, **kwargs)
+        print(time() - before)
+
+
 # 선택 정렬
+@CalcTime
 def selection_sort(num_list):
     for i in range(len(num_list)):
         using_list = num_list[i:]
@@ -19,3 +30,4 @@ def selection_sort(num_list):
 if __name__ == '__main__':
     print("Selection Sort: ")
     after_sort = selection_sort(nums)
+
